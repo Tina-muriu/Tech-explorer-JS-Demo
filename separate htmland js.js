ca828068255b4ce8481c0f3d451323b025e691f9
Tina-muriu <97758217+Tina-muriu@users.noreply.github.com>
2023-10-13 11:18:59 +0300

    Add files via upload
    A repository for JavaScript and HTML projects, demonstrating my coding skills and web development creations. Explore the world of web technologies through a variety of interactive and informative projects.

---
+++ separate htmland js.js
@@ -0,0 +1,24 @@
+
+    document.getElementById("numericsection").innerHTML = 30;
+    document.getElementById("textsection").innerHTML = "Name: James";
+  var a;
+a=10;
+var b, c;
+b=c=20;
+x=a+30;
+console.log("value of a:"+a);
+console.log("value of b:"+b);
+console.log("value of c:"+c);
+console.log("value of x:"+x);
+y=b*c;
+console.log("value of y:"+y);
+Studentname="Brianna";
+studentName="Teresia";
+Student_name="David";
+console.log("The variable(Upper Camel case)"+Studentname);
+console.log("The variable (Lower Camel case"+studentName);
+console.log("The variable(Underscore)"+Student_name);
+singleQuoteString='Declared within single Quotes';
+backtickString='Declared within back ticks';
+console.log('the singleQuoteString:%s',singleQuoteString);
+console.log('The backtickString:{backtickString}')
